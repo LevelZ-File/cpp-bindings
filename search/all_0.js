88ce974e29ff5c0e8a2e42913bf06b401726edc6
GitHub Action <action@github.com>
2024-05-21 16:27:05 +0000

    Update Docs (3bf829d)

--- search/all_0.js
+++ search/all_0.js
@@ -1,7 +1,6 @@
 var searchData=
 [
-  ['block_0',['Block',['../structLevelZ_1_1Block.html',1,'LevelZ']]],
+  ['block_0',['Block',['../structLevelZ_1_1Block.html',1,'LevelZ::Block'],['../structLevelZ_1_1Block.html#a73e0e61ffafacd2e64691cbd35f03c30',1,'LevelZ::Block::Block(std::string name)'],['../structLevelZ_1_1Block.html#ab4e5bbfe001dbed6e64d7a5d43b2eff3',1,'LevelZ::Block::Block(std::string name, std::unordered_map&lt; std::string, std::string &gt; properties)']]],
   ['block_1',['block',['../structLevelZ_1_1LevelObject.html#abeec6bc09ce65f284b1d0cd6cb505d57',1,'LevelZ::LevelObject']]],
-  ['block_2',['Block',['../structLevelZ_1_1Block.html#a73e0e61ffafacd2e64691cbd35f03c30',1,'LevelZ::Block::Block(std::string name)'],['../structLevelZ_1_1Block.html#ab4e5bbfe001dbed6e64d7a5d43b2eff3',1,'LevelZ::Block::Block(std::string name, std::unordered_map&lt; std::string, std::string &gt; properties)']]],
-  ['blocks_3',['blocks',['../structLevelZ_1_1Level.html#a58c8d0c973d40a4a64205ce3a3f71a51',1,'LevelZ::Level']]]
+  ['blocks_2',['blocks',['../structLevelZ_1_1Level.html#a58c8d0c973d40a4a64205ce3a3f71a51',1,'LevelZ::Level']]]
 ];
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['block_0',['block',['../structLevelZ_1_1LevelObject.html#abeec6bc09ce65f284b1d0cd6cb505d57',1,'LevelZ::LevelObject']]],
-  ['block_1',['Block',['../structLevelZ_1_1Block.html#a73e0e61ffafacd2e64691cbd35f03c30',1,'LevelZ::Block::Block(std::string name)'],['../structLevelZ_1_1Block.html#ab4e5bbfe001dbed6e64d7a5d43b2eff3',1,'LevelZ::Block::Block(std::string name, std::unordered_map&lt; std::string, std::string &gt; properties)']]],
+  ['block_0',['Block',['../structLevelZ_1_1Block.html#a73e0e61ffafacd2e64691cbd35f03c30',1,'LevelZ::Block::Block(std::string name)'],['../structLevelZ_1_1Block.html#ab4e5bbfe001dbed6e64d7a5d43b2eff3',1,'LevelZ::Block::Block(std::string name, std::unordered_map&lt; std::string, std::string &gt; properties)']]],
+  ['block_1',['block',['../structLevelZ_1_1LevelObject.html#abeec6bc09ce65f284b1d0cd6cb505d57',1,'LevelZ::LevelObject']]],
   ['blocks_2',['blocks',['../structLevelZ_1_1Level.html#a58c8d0c973d40a4a64205ce3a3f71a51',1,'LevelZ::Level']]]
 ];
